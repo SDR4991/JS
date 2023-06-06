0e0b9e369d341529d6af501de97d0c6559e8e845
SDR <sidrsidr2@gmai.com>
2023-06-06 22:52:02 +0300

    2nd practice

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@
 
 'use strict';
 
-const numbeOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?','');
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?','');
 
 const personalMovieDB = {
 	count : numberOfFilms,
@@ -32,12 +32,28 @@ const personalMovieDB = {
 	genres :[],
 	privat : false
 };
-const a = +prompt('Один из последних просмотренных фильмов?','');
-	b = +prompt ('На сколько оцените его?','');
-	c = +prompt ('Один из последних просмотренных фильмов?','');
-	d = +prompt ('На сколько оцените его?','');	
-	
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-	
+
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?','');
+	const b = prompt ('На сколько оцените его?','');	
+	if( a != null && b !=null && a != '' && b !='' && a.length < 50){
+		personalMovieDB.movies[a] = b;
+		console.log('Done');		
+	}	else{
+		console.log('error');
+		i--;
+	}
+}
+
+if (personalMovieDB.count < 10){
+	alert('Просмотрено довольно мало фильмов');
+}	else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+	alert('Вы классический зритель');
+}	else if(personalMovieDB.count <= 30){
+	alert('Вы киноман');
+}	else {
+	alert('Error');
+}
+
 console.log(personalMovieDB);
